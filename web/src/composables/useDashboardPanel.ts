4bfdcaf47c7acaa08307e0442f36f539900e6e01
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2023-12-28 12:55:18 +0530

    switching from maps to geomap remove fields

--- web/src/composables/useDashboardPanel.ts
+++ web/src/composables/useDashboardPanel.ts
@@ -665,6 +665,10 @@ const useDashboardPanelData = () => {
         dashboardPanelData.data.queries[
           dashboardPanelData.layout.currentQueryIndex
         ].fields.filter = [];
+        dashboardPanelData.data.queries?.forEach((query: any) => {
+          query.fields.name = null;
+          query.fields.value = null;
+        });
         dashboardPanelData.data.htmlContent = "";
         dashboardPanelData.data.markdownContent = "";
         dashboardPanelData.data.queries?.forEach((query: any) => {
